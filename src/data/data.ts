c87189c13bf338f880e36f71eb7f34f39b2b4600
Franklin Shera <fshera96@gmail.com>
2022-12-02 10:06:39 +0300

    contact

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,3 +1,57 @@
+export const contacts: Contact[] = [
+  {
+    headText: "Kassel",
+    name: "Lukas Treu",
+    imageUrl: "/images/lukas.png",
+    address: `Falderbaumstraße 9A
+34123 Kassel`,
+    phone: "0179-4219073",
+    tel: "0561-82023829",
+    email: "bewerbung@Exzellent-Marketing.de",
+  },
+  {
+    headText: "Göttingen",
+    name: "Johannes Treu",
+    imageUrl: "/images/jotreu.png",
+    address: `Rudolf-Wissel-Str. 14
+    37079 Göttingen`,
+    phone: "0151-56168899",
+    tel: "0151-56168899",
+    email: "Goettingen@Exzellent-Marketing.de",
+  },
+  {
+    headText: "Paderborn",
+    name: "Abdur Rahman",
+    imageUrl: "/images/abdur.png",
+    address: ` Warburger Str. 81
+    33098 Paderborn`,
+    phone: "0179-4219073",
+    tel: "-",
+    email: "Paderborn@Exzellent-Marketing.de",
+  },
+
+  {
+    headText: "Bad Homburg",
+    name: "Jonathan",
+    imageUrl: "/images/jonathan.png",
+    address: `Hessenring 89
+    61348 Bad Homburg`,
+    phone: "0179-4219073",
+    tel: "-",
+    email: "BadHomburg@Exzellent-Marketing.de",
+  },
+  {
+    headText: "Münster",
+    name: "Kerim",
+    imageUrl: "/images/kerim.png",
+    address: `Schuckertstraße 33
+    48153 Münster`,
+    phone: "0179-4219073",
+    tel: "-",
+    email: "Muenster@Exzellent-Marketing.de",
+  },
+];
+
 export const stages: IStage[] = [
   {
     title: "Kundenberater",
--- src/env.d.ts
+++ src/env.d.ts
@@ -4,3 +4,13 @@ interface IStage {
   imageUrl: string;
   info: string;
 }
+
+interface Contact {
+  headText: string;
+  imageUrl: string;
+  name: string;
+  address: string;
+  phone: string;
+  tel: string;
+  email: string;
+}
